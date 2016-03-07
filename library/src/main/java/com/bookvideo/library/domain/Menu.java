288ca475cd08991528882cab092183e56353c4d6
Hitoshi Kamezaki <kamezaki@ap-com.co.jp>
2016-03-07 18:08:55 +0900

    add menu

---
+++ library/src/main/java/com/bookvideo/library/domain/Menu.java
@@ -0,0 +1,59 @@
+package com.bookvideo.library.domain;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.util.Collections;
+import java.util.List;
+
+public class Menu {
+    @SerializedName("restId")
+    private String restaurantId;
+    private Style style;
+    private List<Page> pages;
+
+    public String getRestaurantId() {
+        return restaurantId;
+    }
+
+    public Style getStyle() {
+        return style;
+    }
+
+    public List<Page> getPages() {
+        return pages != null ? Collections.unmodifiableList(pages) : Collections.<Page>emptyList();
+    }
+
+    public static class Page {
+        private String title;
+        private int order;
+        // TODO duration
+        private List<Item> categories;
+
+        public String getTitle() {
+            return title;
+        }
+
+        public int getOrder() {
+            return order;
+        }
+
+        public List<Item> getCategories() {
+            return categories != null ? Collections.unmodifiableList(categories) : Collections.<Item>emptyList();
+        }
+
+    }
+
+    public static class Item {
+        @SerializedName("id")
+        private String categoryId;
+        private int order;
+
+        public String getCategoryId() {
+            return categoryId;
+        }
+
+        public int getOrder() {
+            return order;
+        }
+    }
+}
---
+++ library/src/test/java/com/bookvideo/library/domain/MenuTest.java
@@ -0,0 +1,90 @@
+package com.bookvideo.library.domain;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.junit.Assert.assertThat;
+
+public class MenuTest {
+    private Gson gson;
+
+    @Before
+    public void setUp() throws Exception {
+        GsonBuilder gsonBuilder = new GsonBuilder();
+        new StyleConverterAdapter().apply(gsonBuilder);
+        new MenuConverterAdapter().apply(gsonBuilder);
+        gson = gsonBuilder.create();
+    }
+
+    @Test
+    public void shouldDeserializeJson() throws Exception {
+        Menu menu;
+        try (InputStream is = getClass().getResourceAsStream("/menu/menu_simple.json");
+             InputStreamReader reader = new InputStreamReader(is)) {
+            menu = gson.fromJson(reader, Menu.class);
+        }
+
+        assertThat(menu, is(notNullValue()));
+        assertThat(menu.getRestaurantId(), is("menu_id"));
+        {
+            // style
+            Style style = menu.getStyle();
+            assertThat(style, is(notNullValue()));
+            assertThat(style.getLabel().getBackgroundColor(), is(2));
+            assertThat(style.getLabel().getColor(), is(3));
+            assertThat(style.getLabel().getAlign(), is(Style.Align.CENTER));
+            assertThat(style.getLabel().getStyle(), is(Style.LabelStyle.HEIGHT));
+        }
+        assertThat(menu.getPages().size(), is(2));
+        {
+            // page1
+            Menu.Page page = menu.getPages().get(0);
+            assertThat(page.getTitle(), is("page1"));
+            assertThat(page.getOrder(), is(10));
+            {
+                // categories
+                assertThat(page.getCategories().size(), is(1));
+                {
+                    // item 1
+                    Menu.Item categoryItem = page.getCategories().get(0);
+                    assertThat(categoryItem, is(notNullValue()));
+                    assertThat(categoryItem.getCategoryId(), is("category 1-1"));
+                    assertThat(categoryItem.getOrder(), is(1));
+
+                }
+            }
+        }
+        {
+            // page 2
+            Menu.Page page = menu.getPages().get(1);
+            assertThat(page.getTitle(), is("page2"));
+            assertThat(page.getOrder(), is(20));
+            {
+                // categories
+                assertThat(page.getCategories().size(), is(2));
+
+                {
+                    // item 1
+                    Menu.Item categoryItem = page.getCategories().get(0);
+                    assertThat(categoryItem.getCategoryId(), is("category 2-1"));
+                    assertThat(categoryItem.getOrder(), is(2));
+                }
+                {
+                    // item 2
+                    Menu.Item categoryItem = page.getCategories().get(1);
+                    assertThat(categoryItem.getCategoryId(), is("category 2-2"));
+                    assertThat(categoryItem.getOrder(), is(3));
+                }
+            }
+        }
+
+    }
+}
