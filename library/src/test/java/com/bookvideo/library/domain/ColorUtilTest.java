0bdfd2facdcd1a59f34a4af65be8a98501ba6086
Hitoshi Kamezaki <kame@info-cloud.biz>
2016-03-26 09:10:56 +0900

    implement color converter

---
+++ library/src/test/java/com/bookvideo/library/domain/ColorUtilTest.java
@@ -0,0 +1,55 @@
+package com.bookvideo.library.domain;
+
+
+import org.junit.Test;
+import org.junit.experimental.theories.DataPoints;
+import org.junit.experimental.theories.Theories;
+import org.junit.experimental.theories.Theory;
+import org.junit.runner.RunWith;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(Theories.class)
+public class ColorUtilTest {
+
+
+    @DataPoints
+    public static Fixture[] fixtures = {
+            new Fixture("rgba(255,255,255,1)", 0xffffffff),
+            new Fixture("rgba( 255 , 255 , 255 , 1.0 )", 0xffffffff),
+            new Fixture("rgba(255,255,255,0.0)", 0x00ffffff),
+            new Fixture("rgba(255,255,255,0)", 0x00ffffff),
+            new Fixture("rgba(255,255,255,0.5)", 0x7fffffff),
+            new Fixture("rgba(127,0,0,1.0)", 0xff7f0000),
+            new Fixture("rgba(0,127,0,1.0)", 0xff007f00),
+            new Fixture("rgba(0,0,127,1.0)", 0xff00007f),
+            new Fixture("#ffffffff", 0xffffffff),
+            new Fixture("#00ffffff", 0x00ffffff),
+            new Fixture("#00FFFFFF", 0x00ffffff),
+            new Fixture("#10203040", 0x10203040),
+            new Fixture("0xffffffff", 0xffffffff),
+            new Fixture("0x00ffffff", 0x00ffffff),
+            new Fixture("0x00FFFFFF", 0x00ffffff),
+            new Fixture("0x10203040", 0x10203040),
+    };
+
+    @Theory
+    public void colorTest(Fixture fixture) throws Exception {
+
+        assertThat(ColorUtil.fromString(fixture.rgba))
+                .as("verify %s", fixture.rgba)
+                .isEqualTo(fixture.expect);
+
+    }
+
+    static class Fixture {
+        public String rgba;
+        public int expect;
+
+        public Fixture(String rgba, int expect) {
+            this.rgba = rgba;
+            this.expect = expect;
+        }
+
+    }
+}
